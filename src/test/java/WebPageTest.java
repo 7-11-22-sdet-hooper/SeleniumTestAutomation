52a0c217196ff245fc1b14e5fe00e1c000bcbb7b
Terrence <tmitc@users.noreply.github.com>
2022-08-11 15:55:31 -0500

    removed unnecessary imports and variable

--- src/test/java/WebPageTest.java
+++ src/test/java/WebPageTest.java
@@ -1,28 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.JavascriptExecutor;
-
-import java.util.List;
-import java.util.concurrent.*;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 class WebPageTest 
 {
-	  private static WebDriver driver = null;
 	@Test
 	public void testWebPageTitle()
 	{
