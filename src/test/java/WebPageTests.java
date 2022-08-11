17d76dd87c287cfe34e2b758bd02dc0e122d6463
Terrence <tmitc@users.noreply.github.com>
2022-08-11 01:08:53 -0500

    Modified name of class
    Changed name of test class to WebPageTests

--- src/test/java/WebPageTests.java
+++ src/test/java/WebPageTests.java
@@ -3,16 +3,19 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-class MavenExampleTest 
+class WebPageTests 
 {
 	@Test
-	public void testGoogle()
+	public void testWebPageTitle()
 	{
         System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://bespoke-scone-ba3a56.netlify.app/");
 		String title = driver.getTitle();
+		
+		//the title of flash card web page is blank for now so this should be true....
 		assertEquals(title, "");
+		
 		//System.out.println(title);
 		driver.quit();
 	}
