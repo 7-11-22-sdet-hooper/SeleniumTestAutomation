7db056c02f647479d3fca47c2519a1f78ae4765e
Terrence <tmitc@users.noreply.github.com>
2022-08-11 22:46:41 -0500

    Added beforeEach and afterEach annotated methods

--- src/test/java/WebPageTest.java
+++ src/test/java/WebPageTest.java
@@ -1,4 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,55 +9,54 @@ import org.openqa.selenium.By;
 
 class WebPageTest 
 {
+	WebDriver driver;
+
 	// TODO Add method(s) to test cards for a blank or empty titles.
 	// TODO Modify CardDefinition methods to test card title against definition from Database.
+	
+	@BeforeEach public void beforeTest()
+	{
+		  System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
+		  driver = new ChromeDriver();
+		  driver.get("https://bespoke-scone-ba3a56.netlify.app/");
+	}
+	
+	@AfterEach public void afterTest()
+	{
+		driver.quit();
+	}
+	
 	@Test
 	public void testWebPageTitle()
 	{
-        System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://bespoke-scone-ba3a56.netlify.app/");
 		String title = driver.getTitle();
 		
 		//the title of flash card web page is blank for now so this should be true....
 		assertEquals(title, "");
-		driver.quit();
 	}
 
 	@Test
 	public void testBooleanCardName()
 	{
-        String cardName = "";
-        System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://bespoke-scone-ba3a56.netlify.app/");
+        String cardName = "";  
 		cardName = driver.findElement(By.xpath("//*[@id=\"1\"]")).getText();
 		assertEquals(cardName, "boolean");
-		driver.quit();
 	}
 
 	@Test
 	public void testStringCardName()
 	{
         String cardName = "";
-        System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://bespoke-scone-ba3a56.netlify.app/");
 		cardName = driver.findElement(By.xpath("//*[@id=\"2\"]")).getText();		
 		assertEquals(cardName, "string");
-		driver.quit();
 	}
 
 	@Test
 	public void testIntegerCardName()
 	{
         String cardName = "";
-        System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://bespoke-scone-ba3a56.netlify.app/");
 		cardName = driver.findElement(By.xpath("//*[@id=\"3\"]")).getText();		
 		assertEquals(cardName, "integer");
-		driver.quit();
 	}
 
 	@Test
@@ -63,9 +65,6 @@ class WebPageTest
 		final int MAXCHARS = 12;	//max number of letters that will fit on a card without hanging off
         String cardName = "";
         boolean tooManyChar;
-        System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://bespoke-scone-ba3a56.netlify.app/");
 		cardName = driver.findElement(By.xpath("//*[@id=\"3\"]")).getText();
 		
 		if (cardName.length() > MAXCHARS)
@@ -74,53 +73,35 @@ class WebPageTest
 			tooManyChar = false;
 		
 		assertFalse(tooManyChar);
-		driver.quit();
 	}
 	
 	@Test
 	public void testIntegerCardDefinition()
 	{
         String cardDef = "";
-        System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://bespoke-scone-ba3a56.netlify.app/");
-
 	    driver.findElement(By.id("3")).click();	
 	     
 		cardDef = driver.findElement(By.xpath("//*[@id=\"3\"]/div/p")).getText();		
 		assertEquals(cardDef, "numbers without a fractional component, and don't support decimal points");
-		driver.quit();
 	}
 	
 	@Test
 	public void testStringCardDefinition()
 	{
         String cardDef = "";
-        System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://bespoke-scone-ba3a56.netlify.app/");
-
 	    driver.findElement(By.id("2")).click();	
 
-		cardDef = driver.findElement(By.xpath("//*[@id=\"2\"]/div/p")).getText();		
-		
+		cardDef = driver.findElement(By.xpath("//*[@id=\"2\"]/div/p")).getText();
 		assertEquals(cardDef, "series of characters that are interpreted literally by a script");
-		driver.quit();
 	}
 	
 	@Test
 	public void testBooleanCardDefinition()
 	{
         String cardDef = "";
-        System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://bespoke-scone-ba3a56.netlify.app/");
-		
 	    driver.findElement(By.id("1")).click();
 
-		cardDef = driver.findElement(By.xpath("//*[@id=\"1\"]/div/p")).getText();		
-		
+		cardDef = driver.findElement(By.xpath("//*[@id=\"1\"]/div/p")).getText();
 		assertEquals(cardDef, "logical data type that can have only the values true or false");
-		driver.quit();
 	}
 }
