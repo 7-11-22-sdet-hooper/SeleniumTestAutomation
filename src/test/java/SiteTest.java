912dbe9f6867bac599fd77f02a33e5c55be6d3cc
Terrence <tmitc@users.noreply.github.com>
2022-08-12 13:52:25 -0500

    update filepath of chromedriver

--- src/test/java/SiteTest.java
+++ src/test/java/SiteTest.java
@@ -16,7 +16,7 @@ class SiteTest
 	//These statements will be run before each unit test.
 	@BeforeEach public void beforeTest()
 	{
-		  System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_linux64/chromedriver");
+		  //System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_linux64/chromedriver");
 		  driver = new ChromeDriver();
 		  driver.get("https://bespoke-scone-ba3a56.netlify.app/");
 	}
