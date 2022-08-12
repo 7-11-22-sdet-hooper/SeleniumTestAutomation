d08c8bda22eee38ab732494eae8f1af56a1db9f6
Terrence <tmitc@users.noreply.github.com>
2022-08-11 23:34:23 -0500

    modified test class name

--- src/test/java/SiteTest.java
+++ src/test/java/SiteTest.java
@@ -1,5 +1,4 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,13 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 
-class WebPageTest 
+class SiteTest 
 {
 	WebDriver driver;
 
 	// TODO Add method(s) to test cards for a blank or empty titles.
 	// TODO Modify CardDefinition methods to test card title against definition from Database.
 	
+	//These statements will be run before each unit test.
 	@BeforeEach public void beforeTest()
 	{
 		  System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
@@ -21,11 +21,13 @@ class WebPageTest
 		  driver.get("https://bespoke-scone-ba3a56.netlify.app/");
 	}
 	
+	//This statement will run after each unit test.
 	@AfterEach public void afterTest()
 	{
 		driver.quit();
 	}
 	
+	//Unit Tests
 	@Test
 	public void testWebPageTitle()
 	{
