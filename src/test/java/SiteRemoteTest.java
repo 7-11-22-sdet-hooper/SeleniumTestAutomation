1f9e26f75c67b3ee955801eab29ede2212759ea7
Terrence <tmitc@users.noreply.github.com>
2022-08-19 23:31:02 -0500

    Integrated RemoteWebDriver testing

---
+++ src/test/java/SiteRemoteTest.java
@@ -0,0 +1,61 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+//IMPORTANT: Set your local computers environmental variables SAUCE_USERNAME and SAUCE_ACCESS_KEY before running!
+//See readme.md for more details.
+class SiteRemoteTest 
+{
+	WebDriver driver;
+	
+	//These statements will be run before each test.
+	@BeforeEach public void beforeTest() throws MalformedURLException
+	{
+		ChromeOptions options = new ChromeOptions();
+		options.setPlatformName("Windows 11");
+		options.setBrowserVersion("latest");
+
+		Map<String, Object> sauceOptions = new HashMap<>();
+		sauceOptions.put("username", System.getenv("SAUCE_USERNAME"));
+		sauceOptions.put("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
+		options.setCapability("sauce:options", sauceOptions);
+			
+		//If environmental variables SAUCE_USERNAME and SAUCE_ACCESS_KEY have not yet been set by user
+		if (System.getenv("SAUCE_USERNAME") == null || System.getenv("SAUCE_ACCESS_KEY") == null)
+		{
+			throw new MalformedURLException("ERROR: Please set correct user enviroment variables (view Readme.md)");
+		}
+		else//If environmental variables have been set by user
+		{
+			//build URL string using environmental variables (which contain our user name and access key)
+			String sauceLabURLString = "https://" + System.getenv("SAUCE_USERNAME") + ":" + System.getenv("SAUCE_ACCESS_KEY") + "@ondemand.us-west-1.saucelabs.com:443/wd/hub";
+			URL url = new URL(sauceLabURLString);
+			driver = new RemoteWebDriver(url, options);
+			driver.get("https://bespoke-scone-ba3a56.netlify.app");
+		}
+	}
+	
+	//This statement will run after each test.
+	@AfterEach public void afterTest()
+	{
+		driver.quit();
+	}
+	
+	//Test case
+	@Test
+	public void testWebPageTitle()
+	{
+		String title = driver.getTitle();
+		
+		//the title of flash card web page is blank for now so this should be true....
+		assertEquals("", title);
+	}
+}
